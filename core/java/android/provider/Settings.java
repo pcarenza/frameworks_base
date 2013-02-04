4fc6963fbf965126c8248623b547f775cdeb993a
Lars Greiss <kufikugel@googlemail.com>
2013-02-04 13:51:20 +0100

    Frameworks: Expanded desktop mode (1/2)
    this commit adds expanded desktop mode. You can decide if only
    navigation bar, only status bar or both are hidden when expanded
    desktop was activated.
    
    This commit also sorts the entries a bit more logical and alphabetical
    
    Change-Id: I3fe0b1eee6e13cf10debbcbe5338fa901b785c84
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	policy/src/com/android/internal/policy/impl/GlobalActions.java
    
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3593,10 +3593,17 @@ public final class Settings {
         };
 
          /**
-         * Statusbar State
+         * Whether Expanded desktop is currently running or not
          * @hide
          */
         public static final String EXPANDED_DESKTOP_STATE = "expanded_desktop_state";
+        
+        /**
+         * Expanded desktop mode
+         * 0 = none, 1 = hide only navbar, 2 = hide only statusbar, 3 = hide both
+         * @hide
+         */
+        public static final String EXPANDED_DESKTOP_MODE = "expanded_desktop_mode";
 
         /**
          *
