52108afb5cf771eca035083aeeb242ab8a8fc503
iceandfire <arhamjamal@gmail.com>
2013-06-26 12:49:59 +0530

    Fix profile related derps Signed-off-by: iceandfire <arhamjamal@gmail.com>
    Conflicts:
    	core/java/android/app/ContextImpl.java
    	core/res/res/values/cm_strings.xml
    	policy/src/com/android/internal/policy/impl/GlobalActions.java
    
    Conflicts:
    	core/java/android/app/ConnectionSettings.java

--- core/java/android/app/ContextImpl.java
+++ core/java/android/app/ContextImpl.java
@@ -556,12 +556,7 @@ class ContextImpl extends Context {
                     return new IrdaManager(service);
                 }});
                     
-        registerService(PROFILE_SERVICE, new ServiceFetcher() {
-                public Object createService(ContextImpl ctx) {
-                    final Context outerContext = ctx.getOuterContext();
-                    return new ProfileManager (outerContext, ctx.mMainThread.getHandler());
-               }});
-    }
+            }
 
     static ContextImpl getImpl(Context context) {
         Context nextContext;
